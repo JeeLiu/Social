f990e7e022809d5c7da98ecb7376e5b856eb3980
Xiaosen0624 <dongxiaosen@gmail.com>
2013-06-17 16:14:15 +0800

    add some files

---
+++ Social/Social/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Social
+//
+//  Created by Xiaosen on 13-6-17.
+//  Copyright (c) 2013年 WonderShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Social/Social/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Social
+//
+//  Created by Xiaosen on 13-6-17.
+//  Copyright (c) 2013年 WonderShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
